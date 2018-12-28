e60119b66a22ba565a6217b6370ecd1d175a088a
zhangxiangliang <admin@pushme.top>
2018-12-28 18:39:12 +0800

    feat(App): 修改全局样式、增加主题更换相关函数

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -8,12 +8,14 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     }
 
-    this.globalData = {}
-
+    // 初始化云函数
     wx.cloud.init({
       env: config.cloud.env,
       traceUser: true,
     })
+
+    // GlobalData 初始化
+    this.initGlobalData() 
 
     // 设置 openid
     wx.cloud.callFunction({
@@ -21,11 +23,32 @@ App({
       complete: res => this.globalData.openid = res.result.openid,
     })
 
+    // 更换主题方法
+    this.globalData.changeTheme = function () {
+      this.theme.items.forEach(i => wx.setTabBarItem(i))
+      wx.setTabBarStyle(this.theme.tabBarStyle)
+      wx.setNavigationBarColor(this.theme.navigationBar)
+    }
+
     // TODO 设置 publickey
     this.globalData.publicKey = 'pushmetop'
     this.globalData.privateKey = 'pushmetop'
-
-    // 设置数据库
-    this.globalData.collections = config.cloud.collections
+    this.globalData.themeName = config.themeName
   },
+
+  initGlobalData() {
+    this.globalData = { ...config }
+
+    Object.defineProperty(this.globalData, 'themeName', {
+      get: function () {
+        this.changeTheme()
+        return this.realThemeName
+      },
+      set: function (name) {
+        this.realThemeName = name
+        this.theme = this.themes[name]
+        this.changeTheme()
+      }
+    })
+  }
 })
