797587d127bc6842238d9c16cc00599bf81476e8
zhangxiangliang <admin@pushme.top>
2018-12-28 18:44:19 +0800

    feat(Config): 增加配置实例代码

--- miniprogram/config-example.js
+++ miniprogram/config-example.js
@@ -4,5 +4,52 @@ export default {
     collections: {
       passwords: 'passwords',
     }
+  },
+  themeName: 'black',
+  themes: {
+    black: {
+      navigationBar: {
+        "frontColor": "#ffffff",
+        "backgroundColor": "#353435"
+      },
+      tabBarStyle: {
+        color: "#4D4C4D",
+        selectedColor: "#ffffff",
+        backgroundColor: "#353435",
+        borderStyle: "white",
+      },
+      items: [
+        {
+          index: 0,
+          selectedIconPath: "images/lock_fill.png"
+        },
+        {
+          index: 1,
+          selectedIconPath: "images/setup_fill.png"
+        },
+      ]
+    },
+    white: {
+      navigationBar: {
+        frontColor: "#000000",
+        backgroundColor: "#ffffff",
+      },
+      tabBarStyle: {
+        color: "#4D4C4D",
+        selectedColor: "#555555",
+        backgroundColor: "#ffffff",
+        borderStyle: "black",
+      },
+      items: [
+        {
+          index: 0,
+          selectedIconPath: "images/white_lock_fill.png"
+        },
+        {
+          index: 1,
+          selectedIconPath: "images/white_setup_fill.png"
+        },
+      ],
+    },
   }
 }
