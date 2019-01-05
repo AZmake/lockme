d60833822ee7a570cc2bd986a20a2b0d1ea460f0
zhangxiangliang <admin@pushme.top>
2019-01-05 15:09:53 +0800

    feat(Component): 增加 SafeListMessage 用于显示列表提示

---
+++ miniprogram/components/SafeListMessage/index.js
@@ -0,0 +1,30 @@
+// components/SafeListMessage/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    message: {
+      type: String,
+      value: '╮(╯_╰)╭ 暂无数据，您可以进行创建',
+    },
+    theme: {
+      type: String,
+      value: 'black',
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
