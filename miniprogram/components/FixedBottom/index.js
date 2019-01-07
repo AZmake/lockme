40028768d4e4ac114a49bd9aae76e3937c99b539
zhangxiangliang <admin@pushme.top>
2019-01-07 20:19:17 +0800

    feat(Component): 增加 fixed-bottom 布局组件

---
+++ miniprogram/components/FixedBottom/index.js
@@ -0,0 +1,30 @@
+// components/FixedBottom/index.js
+Component({
+  options: {
+    multipleSlots: true
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    padding: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
