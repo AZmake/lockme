e6c690689fffb5a8cdde59bdf8845fdebbadeae7
zhangxiangliang <admin@pushme.top>
2019-01-08 16:15:10 +0800

    feat(Component): 增加 paragraph 组件用于显示段落

---
+++ miniprogram/components/Paragraph/index.js
@@ -0,0 +1,33 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: '标题',
+    },
+    content: {
+      type: String,
+      value: '内容',
+    },
+    theme: {
+      type: String,
+      value: 'black',
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
