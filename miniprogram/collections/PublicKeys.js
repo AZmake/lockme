99fb5b5a0ebab51d9369552448e3ef5bb5dd0f13
zhangxiangliang <admin@pushme.top>
2019-01-11 00:28:37 +0800

    feat(Collection): 完成公钥集合和公钥模型

---
+++ miniprogram/collections/PublicKeys.js
@@ -0,0 +1,45 @@
+import Collection from './Collection'
+import PublicKey from '../models/PublicKey'
+
+export class PublicKeyCollection extends Collection {
+  constructor() {
+    super('publicKeys')
+  }
+
+  get() {
+    return this.getToast().then(res => {
+      this.items = res.data.map(i => new PublicKey(i))
+      return this.items
+    })
+  }
+
+  getOne() {
+    return this.get().then(res => res.length > 0 ? res[0] : null)
+  }
+
+  add(item) {
+    return this.addToast(item).then(res => {
+      item._id = res._id
+      return this.setItems([item, ...this.items]).uniqueById()
+    })
+  }
+
+  edit(item) {
+    return this.editToast(item)
+      .then(() => {
+        return this.setItems([item, ...this.items]).uniqueById()
+      })
+  }
+
+  remove(item) {
+    return this.removeToast(item)
+  }
+
+  toJson() {
+    return {
+      value: this.value
+    }
+  }
+}
+
+export default new PublicKeyCollection;
---
+++ miniprogram/models/PublicKey.js
@@ -0,0 +1,16 @@
+import Model from './Model'
+
+export default class PublicKey extends Model {
+  constructor(item = {}) {
+    super(item)
+
+    this._id = item._id
+    this.value = item.value
+  }
+
+  toJson() {
+    return {
+      value: this.value,
+    }
+  }
+}
