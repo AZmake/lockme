9b6ce24f40efaca18281d99566f71bc577ba570f
zhangxiangliang <admin@pushme.top>
2019-01-11 00:32:09 +0800

    feat(Component): 增加段落组件的文本输入模式

--- miniprogram/components/Paragraph/index.js
+++ miniprogram/components/Paragraph/index.js
@@ -15,19 +15,34 @@ Component({
       type: String,
       value: 'black',
     },
+    istextarea: {
+      type: Boolean,
+      value: false
+    },
+    placeholder: {
+      type: String,
+      value: '',
+    },
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    value: '',
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    setValue(e) {
+      const value = e.detail.value
+      this.setData({value: value })
+      this.triggerEvent(`setprivatekey`, { value })
+    },
+    tap() {
+      this.triggerEvent(`click`)
+    }
   }
 })
