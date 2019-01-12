c4110ab308ae409fde2bb83bc2d50d0d9dd6530b
zhangxiangliang <admin@pushme.top>
2019-01-12 18:24:38 +0800

    feat(Util): 增加主题和密码长度、检测间隔 常量

--- miniprogram/utils/Const.js
+++ miniprogram/utils/Const.js
@@ -8,6 +8,25 @@ export const REG_UPPERCASE = /[A-Z]/
 export const REG_NUMBER    = /[0-9]/
 export const REG_SPECIAL   = /[-.~!@#$%^&*()_:<>,?]/
 
+// 应用密码长度
+export const FACEPASS_LENGTH  = 6
+
+// 应用密码检测间隔时间
+export const VALID_TIME = [
+  { value: 30 * 1000, text:'30秒' },
+  { value: 1 * 60 * 1000, text: '1分钟' },
+  { value: 3 * 60 * 1000, text: '3分钟' },
+  { value: 5 * 60 * 1000, text: '5分钟' },
+  { value: 10 * 60 * 1000, text: '10分钟' },
+  { value: 0, text: '永不' },
+]
+
+// 主题
+export const THEMES = [
+  { value: 'black', text: '黑暗主题' },
+  { value: 'white', text: '纯白主题' },
+]
+
 export default {
   LOWERCASE,
   UPPERCASE,
@@ -17,4 +36,7 @@ export default {
   REG_UPPERCASE,
   REG_NUMBER,
   REG_SPECIAL,
+  VALID_TIME,
+  THEMES,
+  FACEPASS_LENGTH,
 }
