03ff162a3911d6a9bc51eee719f41c03d2ac1c43
zhangxiangliang <admin@pushme.top>
2019-01-12 19:52:53 +0800

    feat(Model): 增加 Model 中 db 的初始化

--- miniprogram/models/Model.js
+++ miniprogram/models/Model.js
@@ -1,9 +1,17 @@
 import Base from '../utils/Base'
+import Config from '../utils/Config'
 
 export default class Model extends Base {
   constructor(item) {
     super()
-  
+    
+    wx.cloud.init({
+      env: Config.cloud.env,
+      traceUser: true,
+    })
+
+    this._db = wx.cloud.database()
+
     this.created_at = item.created_at
     this.updated_at = item.updated_at
   }
