38cedcbc04c31f7fec3ab0f641d32459b71c016c
zhangxiangliang <admin@pushme.top>
2019-01-24 02:08:42 +0800

    feat(Page): 完成更换私钥

---
+++ miniprogram/pages/change-private-key/index.js
@@ -0,0 +1,105 @@
+import Base from '../../utils/Base'
+import Crypto from '../../utils/Crypto'
+import { Safes } from '../../collections/Safes'
+import { PublicKeys } from '../../collections/PublicKeys'
+import Safe from '../../models/Safe';
+
+
+const app = getApp()
+const base = new Base
+
+Page({
+  data: {
+    theme: '',
+    privateKey: '',
+    isChange: false,
+  },
+
+  onLoad() {
+    this.setData({
+      crypto: Crypto.sm2.generateKeyPairHex()
+    })
+  },
+
+  onShow() {
+    this.setData({
+      theme: app.globalData.theme,
+    })
+    app.pageShow()
+  },
+
+  setPrivateKey(e) {
+    this.setData({ privateKey: e.detail.value })
+  },
+
+  changePrivateKey() {
+    if(this.data.isChange) {
+      base._toast("请保存好新私钥")
+    } else if (this.checkPrivateKey()) {
+      wx.showModal({
+        title: '提示',
+        content: '更换私钥后原私钥将失效',
+        success: res => res.cancel || this.updateSafes()
+      })
+    }
+  },
+
+  updateSafes() {
+    const crypto = this.data.crypto
+
+    // 提示
+    base._loading("更新中请勿退出")
+
+    // 更新密码
+    Safes.get().then(items => {
+      let counter = 0
+
+      // 批量更新
+      items.forEach((item) => {
+        item.crypto = crypto
+
+        // 由于是异步更新，需要等更新完后再存储新私钥
+        Safes.edit(item).then(() => {
+          counter++
+          if(counter == items.length) {
+            // 更新公钥
+            PublicKeys.get().then(item => {
+              item.value = crypto.publicKey
+              PublicKeys.edit(item)
+            })
+
+            // 更新本地存储
+            wx.setStorageSync('crypto', crypto)
+            this.setData({ isChange: true })
+            base._toast('私钥已更换成功')
+          }
+        })
+      })
+    })
+  },
+
+  checkPrivateKey() {
+    const publicKey = app.publicKey.value
+    const privateKey = this.data.privateKey
+
+    if (privateKey == '') {
+      return base._toast('原私钥不能为空')
+    }
+
+    base._loading('校验原私钥和公钥是否匹配')
+
+    const check = Crypto.sm2.getPublicKeyFromPrivateKey(privateKey)
+
+    if(check == publicKey) {
+      wx.hideLoading()
+      return true
+    } else {
+      base._toast('原私钥和公钥不匹配，请确认私钥是否正确')
+      return false
+    }
+  },
+
+  copyNewPrivateKey() {
+    wx.setClipboardData({ data: this.data.crypto.privateKey })
+  }
+})
